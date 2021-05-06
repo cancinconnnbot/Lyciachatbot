a8e3fae732d68c0d51bd257116ba1f21b8b32e0b
cancinconnnbot <81495310+cancinconnnbot@users.noreply.github.com>
2021-05-06 17:03:20 +0300

    Update sample_config.py

--- lycia/sample_config.py
+++ lycia/sample_config.py
@@ -1,4 +1,4 @@
 class Config(object):
-    API_ID = int("api_id")
-    API_HASH = "api_hash"
-    TOKEN = "token"
+    API_ID = int("3926716")
+    API_HASH = "2d0c249f0efe0fe1ea2551703a2f774d"
+    TOKEN = "1837880269:AAGnwgAYKwHNVxQq4i1wCoKyBQ1VoUWrhhs"
