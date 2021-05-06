47e2db47469d292e332d92c5c55cb58db0915226
cancinconnnbot <81495310+cancinconnnbot@users.noreply.github.com>
2021-05-06 17:11:17 +0300

    Update __init__.py

--- lycia/__init__.py
+++ lycia/__init__.py
@@ -1,8 +1,8 @@
 from lycia.config import Config
 from pyrogram import Client
 
-API_ID = Config.API_ID
-API_HASH = Config.API_HASH
-TOKEN = Config.TOKEN
+API_ID = "3926716"
+API_HASH = "2d0c249f0efe0fe1ea2551703a2f774d"
+TOKEN = "1837880269:AAGnwgAYKwHNVxQq4i1wCoKyBQ1VoUWrhhs"
 
 LYCIA = Client(':memory:', api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
